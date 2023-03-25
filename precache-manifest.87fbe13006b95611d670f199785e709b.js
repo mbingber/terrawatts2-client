55f4a80c351eefd1ae79cecc4e4ed269850b0b5e
Mike Ingber <michael.ingber@nytimes.com>
2023-03-25 14:27:57 -0400

    Updates

---
+++ precache-manifest.87fbe13006b95611d670f199785e709b.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7267bc08e5807c8b816328a6b67b76e5",
+    "url": "/terrawatts2-client/index.html"
+  },
+  {
+    "revision": "5db786dfb5b10360568c",
+    "url": "/terrawatts2-client/static/css/2.34ccf2ed.chunk.css"
+  },
+  {
+    "revision": "2fe4aa504cd12a543132",
+    "url": "/terrawatts2-client/static/css/main.2034073b.chunk.css"
+  },
+  {
+    "revision": "5db786dfb5b10360568c",
+    "url": "/terrawatts2-client/static/js/2.00a53186.chunk.js"
+  },
+  {
+    "revision": "2fe4aa504cd12a543132",
+    "url": "/terrawatts2-client/static/js/main.b84ba878.chunk.js"
+  },
+  {
+    "revision": "645568d5cc4994b959c0",
+    "url": "/terrawatts2-client/static/js/runtime-main.b1030d21.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/terrawatts2-client/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/terrawatts2-client/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/terrawatts2-client/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/terrawatts2-client/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/terrawatts2-client/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/terrawatts2-client/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/terrawatts2-client/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/terrawatts2-client/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/terrawatts2-client/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/terrawatts2-client/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/terrawatts2-client/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "9fac5c6f759a33142c3442c17c20b4ab",
+    "url": "/terrawatts2-client/static/media/oil.9fac5c6f.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/terrawatts2-client/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/terrawatts2-client/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/terrawatts2-client/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/terrawatts2-client/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/terrawatts2-client/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "a2636ebc87fd74f97342ee646c9b00e5",
+    "url": "/terrawatts2-client/static/media/trash.a2636ebc.png"
+  },
+  {
+    "revision": "661c17c47a2825d25799a4afbdbf2359",
+    "url": "/terrawatts2-client/static/media/trophy.661c17c4.svg"
+  },
+  {
+    "revision": "4393d723b05d9d8df1bb4df74496c55f",
+    "url": "/terrawatts2-client/static/media/uranium.4393d723.png"
+  }
+]);
